6d33b9edf571db1bb968b4ab4fb5ddca086ae5eb
PaoloCampanelli <paolo.campanelli@studentiunicam.it>
2020-12-26 20:33:37 +0100

    Cambiato nome proprietà

--- Project/it/unicam/cs/ids/c3spa/StatoPacco.java
+++ Project/it/unicam/cs/ids/c3spa/StatoPacco.java
@@ -3,6 +3,6 @@ package it.unicam.cs.ids.c3spa;
 import java.util.Date;
 
 public class StatoPacco {
-	private Date _data;
-	private StatoEnum _stato;
+	private Date data;
+	private StatoEnum stato;
 }
