790b389df1445cd79b8d07fb2b99e0ba08ae231f
acata8 <andrea.cataluffi@studenti.unicam.it>
2021-01-02 20:41:03 +0100

    code: creata interfaccia Controller, nuove implementatazioni ConsoleController

---
+++ Project/it/unicam/cs/ids/c3spa/core/ConsoleController.java
@@ -0,0 +1,93 @@
+package it.unicam.cs.ids.c3spa.core;
+
+public class ConsoleController implements Controller{
+
+    @Override
+    public boolean executerCliente(String comando) {
+        if("NEGOZI".equals(comando)) {
+            trovaNegozi();
+        }
+        else if("CATEGORIE".equals(comando)) {
+            trovaCategorie();
+        }
+        else if("PROMOZIONI".equals(comando)) {
+            trovaPromozioni();
+        }
+        else if("EXIT".equals(comando)) {
+            System.exit(0);
+        }else {
+            if(checkList(comando)) {
+                //TODO Stream dei negozi che hanno una data categoria;
+            }else
+                return false;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean executerCommerciante(String comando) {
+        if("AGGIUNGI CATEGORIA".equals(comando) || "AG".contains(comando)) {
+            //TODO
+        }
+        else if("RIMUOVI CATEGORIA".equals(comando) || "RG".contains(comando)) {
+            //TODO
+        }
+        else if("AGGIUNGI PROMOZIONE".equals(comando) || "AP".contains(comando)) {
+            //TODO
+        }
+        else if("RIMUOVI PROMOZIONE".equals(comando) || "RP".contains(comando)) {
+            //TODO
+        }
+        else if("CREA ORDINE".equals(comando) || "OP".contains(comando)) {
+            //TODO
+        }
+        else if("ATTIVA PUBBLICITA".equals(comando) || "AT".contains(comando)) {
+            //TODO
+        }
+        else if("EXIT".equals(comando)) {
+            System.exit(0);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean executerCorriere(String comando) {
+        if("VISUALIZZA ORDINI".equals(comando) || "VO".equals(comando)) {
+            //TODO
+        }
+        else if("PRESA IN CARICO".equals(comando) || "PC".equals(comando)){
+            //TODO
+        }
+        else if("EXIT".equals(comando)) {
+            System.exit(0);
+        }
+        return false;
+    }
+
+    private boolean trovaNegozi() {
+        //TODO
+        return false;
+    }
+
+    private boolean trovaPromozioni() {
+        //TODO
+        return false;
+    }
+
+    private boolean trovaCategorie() {
+        //TODO
+        return false;
+    }
+
+    private boolean checkList(String comando){
+	/*
+		Iterator<Categoria> iteratore = list.iterator();
+		while(iteratore.hasNext()) {
+			Categoria elemento = iteratore.next();
+			if(comando.equals(elemento.getNome())) {
+				return true;
+			}
+	*/
+        return false;
+    }
+}
--- Project/it/unicam/cs/ids/c3spa/core/Controller.java
+++ Project/it/unicam/cs/ids/c3spa/core/Controller.java
@@ -1,34 +1,9 @@
 package it.unicam.cs.ids.c3spa.core;
 
-public class Controller {
+public interface Controller {
 
-
-    public boolean clienteExecuter(String comando) {
-        if("NEGOZI".equals(comando)) {
-            //TODO Stream di negozi dove citta' cliente = citta' negozio
-        }
-        else if("CATEGORIE".equals(comando)) {
-            //TODO Stream di categorie dove citta' cliente = citta' negozio
-        }
-        else if("PROMOZIONI".equals(comando)) {
-            //TODO Stream di promozioni dove citta' cliente = citta' negozio
-        }
-        else if(checkList(comando)) {
-            //TODO Stream di negozi che soddisfano la categoria
-        }
-
-        return false;
-    }
-
-    private boolean checkList(String comando){
-	/* List<Categoria>
-	 *
-		Iterator<Categoria> iteratore = list.iterator();
-		while(iteratore.hasNext()) {
-			Categoria elemento = iteratore.next();
-			if(comando.equals(elemento.getNome())) {
-	*/
-        return false;
-    }
+    boolean executerCliente(String comando);
+    boolean executerCorriere(String comando);
+    boolean executerCommerciante(String comando);
 
 }
