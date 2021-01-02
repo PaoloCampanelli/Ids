e082147dd55347628d5d8af76ce3959d50ee1bd3
Sara <sara.comapgnucci@studenti.unicam.it>
2021-01-02 11:54:35 +0100

    code: creazione costruttore in CategoriaMerceologica

--- Project/it/unicam/cs/ids/c3spa/core/CategoriaMerceologica.java
+++ Project/it/unicam/cs/ids/c3spa/core/CategoriaMerceologica.java
@@ -3,4 +3,9 @@ package it.unicam.cs.ids.c3spa.core;
 public class CategoriaMerceologica {
 	private int idCategoria;
 	private String nome;
+
+	public CategoriaMerceologica(int id, String nome) {
+		this.idCategoria= id;
+		this.nome = nome;
+	}
 }
