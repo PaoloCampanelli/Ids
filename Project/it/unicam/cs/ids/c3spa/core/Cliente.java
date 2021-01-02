5bece23374b1a5f1a4561da15083dca425e4eac6
Sara <sara.comapgnucci@studenti.unicam.it>
2021-01-02 11:19:33 +0100

    code: scrittura metodo creazione Account per i Clienti, aggiunta intero "numeroClienti"

--- Project/it/unicam/cs/ids/c3spa/core/Cliente.java
+++ Project/it/unicam/cs/ids/c3spa/core/Cliente.java
@@ -1,10 +1,21 @@
 package it.unicam.cs.ids.c3spa.core;
 
 import it.unicam.cs.ids.c3spa.core.astratto.Account;
+import it.unicam.cs.ids.c3spa.core.astratto.IGestiscoAccount;
 
 import java.util.List;
 
-public class Cliente extends Account
+public class Cliente extends Account implements IGestiscoAccount
 {
     public List<Pacco> pacchi;
+
+
+    public Cliente(int clienteId, String nomeCognome, Indirizzo indirizzo, String telefono, String eMail, String password) {
+        this.id= clienteId;
+        this.denominazione = nomeCognome;
+        this.indirizzo = indirizzo;
+        this.telefono = telefono;
+        this.eMail = eMail;
+        this.password = password;
+    }
 }
--- Project/it/unicam/cs/ids/c3spa/core/Pubblico.java
+++ Project/it/unicam/cs/ids/c3spa/core/Pubblico.java
@@ -1,5 +1,28 @@
 package it.unicam.cs.ids.c3spa.core;
 
-public class Pubblico {
-	public Cliente clienti;
+import it.unicam.cs.ids.c3spa.core.astratto.Account;
+import it.unicam.cs.ids.c3spa.core.astratto.IGestiscoAccount;
+
+import java.util.List;
+
+public class Pubblico implements IGestiscoAccount{
+	public List<Cliente> clienti;
+	public int numeroClienti;
+
+	@Override
+	public Account CreaAccount(int id, String denominazione, Indirizzo indirizzo, String telefono, String eMail, String password) {
+		if(password.length()<5){
+			throw new IllegalArgumentException("La password deve contenere almeno 6 caratteri");
+		}
+		Cliente accountCliente= new Cliente(id, denominazione, indirizzo, telefono, eMail, password);
+		accountCliente.id= numeroClienti;
+		numeroClienti++;
+		clienti.add(accountCliente);
+		return accountCliente;
+	}
+
+	@Override
+	public void SetPassword(String password) {
+
+	}
 }
