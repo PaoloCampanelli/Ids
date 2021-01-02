ac18e61908dfda64958fb271e906a3b70675eb74
Sara <sara.comapgnucci@studenti.unicam.it>
2021-01-02 11:30:19 +0100

    code: scrittura metodo creazione Account per i Corrieri, creazione costruttore in Corriere, inserimento di un intero "numeroCorrieri", .

--- Project/it/unicam/cs/ids/c3spa/core/Trasportatori.java
+++ Project/it/unicam/cs/ids/c3spa/core/Trasportatori.java
@@ -1,5 +1,29 @@
 package it.unicam.cs.ids.c3spa.core;
 
-public class Trasportatori {
-	public Corriere corrieri;
+import it.unicam.cs.ids.c3spa.core.astratto.Account;
+import it.unicam.cs.ids.c3spa.core.astratto.IGestiscoAccount;
+
+import java.util.List;
+
+public class Trasportatori implements IGestiscoAccount {
+	public List<Corriere> corrieri;
+	int numeroCorrieri;
+
+
+	@Override
+	public Account CreaAccount(int id, String denominazione, Indirizzo indirizzo, String telefono, String eMail, String password) {
+		if(password.length()<5){
+			throw new IllegalArgumentException("La password deve contenere almeno 6 caratteri");
+		}
+		Corriere accountCorriere= new Corriere(id, denominazione, indirizzo, telefono, eMail, password);
+		accountCorriere.id= numeroCorrieri;
+		numeroCorrieri++;
+		corrieri.add(accountCorriere);
+		return accountCorriere;
+	}
+
+	@Override
+	public void SetPassword(String password) {
+
+	}
 }
