7094f9dab09c749afba372e0c9f25d6b5e78ba29
acata8 <andrea.cataluffi@studenti.unicam.it>
2021-01-03 21:24:00 +0100

    UI: Gestione autenticazione

--- Project/it/unicam/cs/ids/c3spa/core/view/ConsoleView.java
+++ Project/it/unicam/cs/ids/c3spa/core/view/ConsoleView.java
@@ -17,41 +17,62 @@ public class ConsoleView implements IView{
         System.out.println("C3 V 1.0\nPowered by C3 SPA");
         System.out.println("----------------");
         System.out.println("Benvenuto!");
-        System.out.println("Digita la tua tipologia di utente (Cliente, Commerciante, Corriere)");
     }
 
     @Override
     public void start() throws IOException {
         hello();
-        String login;
+        autenticazione();
+        String input;
+        System.out.println("Digita la tua tipologia di utente (Cliente, Commerciante, Corriere)");
         do {
-            login = br.readLine().toUpperCase();
-            switch(login){
-                case "CLIENTE":
+            input = br.readLine().toUpperCase();
+            switch(input) {
+                case "CLIENTE":{
                     view = new ViewCliente();
                     view.start();
                     break;
-                case "CORRIERE":
+                }
+                case "CORRIERE":{
                     view = new ViewCorriere();
                     view.start();
                     break;
+                }
                 case "COMMERCIANTE":
-                    view = new ViewCommerciante();
+                    view = new ViewCommerciante();{
                     view.start();
                     break;
+                }
                 case "EXIT":
                     System.exit(0);
                 default:
                     System.err.println("Scelta non valida");
             }
-        }while(!login.equals("EXIT"));
+        }while(!input.equals("EXIT"));
     }
+
+    private void autenticazione() throws IOException {
+        System.out.println("Sei già registrato? Y/N");
+        String input;
+        do {
+            input = br.readLine().toUpperCase();
+            switch(input) {
+                case "N":
+                    //TODO Registrazione utente
+                    break;
+                case "Y":
+                    //TODO Login utente
+                    break;
+                default:
+                    System.err.println("Scelta non valida!");
+            }
+        }while(!input.equals("Y"));
+    }
+
 
     public BufferedReader getBr() {
         return br;
     }
 
-    public ConsoleController getController() {
-        return consoleController;
-    }
+    public ConsoleController getController() { return consoleController; }
 }
