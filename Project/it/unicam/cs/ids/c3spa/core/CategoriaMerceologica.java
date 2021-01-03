55a2100ff073a5606d217bd905d8fea6adef0917
Sara <sara.comapgnucci@studenti.unicam.it>
2021-01-03 19:26:08 +0100

    code: scrittura metodo per la creazione delle categorie

--- Project/it/unicam/cs/ids/c3spa/core/CategoriaMerceologica.java
+++ Project/it/unicam/cs/ids/c3spa/core/CategoriaMerceologica.java
@@ -1,8 +1,8 @@
 package it.unicam.cs.ids.c3spa.core;
 
 public class CategoriaMerceologica {
-	private int idCategoria;
-	private String nome;
+	protected int idCategoria;
+	protected String nome;
 
 	public CategoriaMerceologica(int id, String nome) {
 		this.idCategoria= id;
--- Project/it/unicam/cs/ids/c3spa/core/Negozio.java
+++ Project/it/unicam/cs/ids/c3spa/core/Negozio.java
@@ -8,6 +8,7 @@ public class Negozio extends Account {
 	public int token;
 	public List<CategoriaMerceologica> categorie;
 	public List<Pacco> pacchi;
+	public int numeroCategorie;
 
 	public Negozio(int negozioId, String denominazione, Indirizzo indirizzo, String telefono, String eMail, String password) {
 		this.id= negozioId;
@@ -16,6 +17,18 @@ public class Negozio extends Account {
 		this.telefono = telefono;
 		this.eMail = eMail;
 		this.password= password;
+	}
+
+	public CategoriaMerceologica creaCategoria(int id, String nome) {
+		//verifico che la categoria che voglio aggiungere non sia già presente, se è gia presente la ritorno.
+		if (categorie.stream().anyMatch(c->c.idCategoria==id && c.nome.equals(nome))) {
+			return categorie.stream().filter(c -> c.idCategoria==id).findFirst().orElse(null);
+		}
+		CategoriaMerceologica c= new CategoriaMerceologica(id, nome);
+		c.idCategoria= numeroCategorie;
+		numeroCategorie++;
+		categorie.add(c);
+		return c;
 	}
 
 	public String getIndirizzo() {
