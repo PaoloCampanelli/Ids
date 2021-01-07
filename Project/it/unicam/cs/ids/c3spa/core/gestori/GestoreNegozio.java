189637a9d95319e65cf28cbaa9a8030459a167a0
Sara <sara.comapgnucci@studenti.unicam.it>
2021-01-07 16:22:40 +0100

    code : modifica almetodo di ricerca delle categorie

--- Project/it/unicam/cs/ids/c3spa/core/gestori/GestoreNegozio.java
+++ Project/it/unicam/cs/ids/c3spa/core/gestori/GestoreNegozio.java
@@ -211,22 +211,16 @@ public class GestoreNegozio extends GestoreBase implements ICRUD{
     }
 
     public List<Negozio> getByCategoria(String categoria) throws SQLException {
-        Statement st;
+        PreparedStatement st;
         ResultSet rs;
         String sql;
         List<Negozio> ln = new ArrayList<>();
         Connection conn = ApriConnessione();
 
         try {
-            st = conn.createStatement(); // creo sempre uno statement sulla
-            sql = "SELECT * FROM negozi \n"+
-            "INNER JOIN negozio_categoriemerceologiche ON negozi.negozioId = negozio_categoriemerceologiche.negozioId\n"+
-            "INNER JOIN categoriemerceologiche ON negozio_categoriemerceologiche.categoriaId = categoriemerceologiche.categoriaId\n"+
-            "WHERE ('categoriemerceologiche.nome' like '%"+categoria+"%');";
-            rs = st.executeQuery(sql); // faccio la query su uno statement
-            while (rs.next() == true) {
-                ln.add(new Negozio().mapData(rs));
-            }
+            st = conn.prepareStatement("SELECT * FROM negozi, categoriemerceologiche" +
+                    "WHERE categoriemerceologiche.nome = " + categoria + ";");
+            rs = st.executeQuery(); // faccio la query su uno statement
             st.close();
         } catch (SQLException e) {
             System.out.println("errore:" + e.getMessage());
