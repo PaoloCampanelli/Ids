6922f8f10787854cdc395ff21ba0b113fdcb45a3
Sara <sara.comapgnucci@studenti.unicam.it>
2021-01-08 12:04:14 +0100

    code: modifiche dei metodi di ricerca per mittente e per destinatario su GestorePacco

--- Project/it/unicam/cs/ids/c3spa/core/gestori/GestorePacco.java
+++ Project/it/unicam/cs/ids/c3spa/core/gestori/GestorePacco.java
@@ -162,20 +162,18 @@ public class GestorePacco extends GestoreBase implements ICRUD {
     public List<Pacco> getByDestinatario(String denominazioneCliente) throws SQLException {
         PreparedStatement st;
         ResultSet rs;
-        String sql;
-        Pacco p = new Pacco();
         List<Pacco> lp = new ArrayList<>();
         Connection conn = ApriConnessione();
 
         try {
 
-            st = conn.prepareStatement("SELECT * FROM pacchi\n"+
+            st = conn.prepareStatement("SELECT distinct pacchi.paccoId, `destinatario`, `mittente`, `corriere`, `dataPreparazione`, `dataConsegnaRichiesta`, clienti.denominazione, clienti.eMail, clienti.telefono FROM pacchi\n"+
                     "INNER JOIN clienti ON pacchi.destinatario = clienti.clienteId\n"+
                     "WHERE ('denominazione' like '%"+denominazioneCliente+"%');");
             rs = st.executeQuery(); // faccio la query su uno statement
             while (rs.next() == true) {
-                lp.add(new Pacco().mapData(rs));
-                
+                Pacco p = new Pacco().mapData(rs);
+                lp.add(p);
             }
             st.close();
         } catch (SQLException e) {
@@ -199,7 +197,7 @@ public class GestorePacco extends GestoreBase implements ICRUD {
 
         try {
 
-            st = conn.prepareStatement("SELECT * FROM pacchi\n"+
+            st = conn.prepareStatement("SELECT distinct pacchi.paccoId, `destinatario`, `mittente`, `corriere`, `dataPreparazione`, `dataConsegnaRichiesta`, negozi.denominazione, negozi.eMail, negozi.telefono FROM pacchi\n"+
                     "INNER JOIN negozi ON pacchi.destinatario = negozi.negozioId\n"+
                     "WHERE ('denominazione' like '%"+denominazioneNegozio+"%');");
             rs = st.executeQuery(); // faccio la query su uno statement
