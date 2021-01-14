8dbb4855912498213f907e4cbb7b500812ea04ea
Sara <sara.comapgnucci@studenti.unicam.it>
2021-01-14 10:34:47 +0100

    test: per la classe CategoriaMerceologica

---
+++ Project/it/unicam/cs/ids/c3spa/test/CategoriaMerceologicaTest.java
@@ -0,0 +1,25 @@
+package it.unicam.cs.ids.c3spa.test;
+
+import com.mysql.cj.protocol.Resultset;
+import it.unicam.cs.ids.c3spa.core.CategoriaMerceologica;
+import it.unicam.cs.ids.c3spa.core.Cliente;
+import it.unicam.cs.ids.c3spa.core.Indirizzo;
+import it.unicam.cs.ids.c3spa.core.Negozio;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoriaMerceologicaTest {
+
+    private Negozio negozio= new Negozio();
+
+    @Test
+    public void CategoriaMerceologica() throws SQLException {
+        CategoriaMerceologica categoriaFrutta = new CategoriaMerceologica(1, "FRUTTA");
+        CategoriaMerceologica categoriaAbbigliamento = new CategoriaMerceologica(2, "ABBIGLIAMENTO");
+        assertEquals(categoriaFrutta, negozio.aggiungiCategoria(categoriaFrutta));
+        assertEquals(categoriaAbbigliamento, negozio.aggiungiCategoria(categoriaAbbigliamento));
+    }
+}
