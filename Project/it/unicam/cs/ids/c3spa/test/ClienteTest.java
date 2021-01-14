bd177d51e15525d3a2611845c790c211e474a572
Sara <sara.comapgnucci@studenti.unicam.it>
2021-01-14 12:18:03 +0100

    test: modifiche nei test di cliente.

--- Project/it/unicam/cs/ids/c3spa/test/ClienteTest.java
+++ Project/it/unicam/cs/ids/c3spa/test/ClienteTest.java
@@ -15,9 +15,9 @@ public class ClienteTest {
 
     @Test
     public void Cliente() throws SQLException {
-        Indirizzo indirizzoSara = new Indirizzo().CreaIndirizzo("NONIO BASSO", "14", "URBISAGLIA","62010",  "MC");
+        Indirizzo indirizzoSara = new Indirizzo().CreaIndirizzo("BASSO", "14", "URBISAGLIA","62010",  "MC");
         Cliente clienteSara = new Cliente("SARA COMPAGNUCCI", indirizzoSara, "34111111111", "SARA.COMPAGNUCCI@GMAIL.COM", "SARA!!");
-        Indirizzo indirizzoPaolo = new Indirizzo().CreaIndirizzo("GIROLAMO DI GIOVANNI", "10", "CAMERINO", "62032", "MC");
+        Indirizzo indirizzoPaolo = new Indirizzo().CreaIndirizzo("GIOVANNI", "10", "CAMERINO", "62032", "MC");
         Cliente clientePaolo = new Cliente( "PAOLO CAMPANELLI", indirizzoPaolo, "1111341111", "PAOLO.CAMPANELLI@GMAIL.COM", "PAOLO!!");
         assertEquals(clienteSara, clienteSara.CreaAccount(1, "SARA COMPAGNUCCI", indirizzoSara, "34111111111", "SARA.COMPAGNUCCI@GMAIL.COM", "SARA!!"));
         assertEquals(clientePaolo, clientePaolo.CreaAccount(2, "PAOLO CAMPANELLI", indirizzoPaolo, "1111341111", "PAOLO.CAMPANELLI@GMAIL.COM", "PAOLO!!"));
