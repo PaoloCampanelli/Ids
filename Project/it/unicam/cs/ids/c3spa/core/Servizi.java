4511b2c9a08b4bf01f92da7b6a69b37721cc2986
Sara <sara.comapgnucci@studenti.unicam.it>
2021-02-01 23:05:05 +0100

    code: scrittura dei metodi per criptare/decriptare le password

--- Project/it/unicam/cs/ids/c3spa/core/Servizi.java
+++ Project/it/unicam/cs/ids/c3spa/core/Servizi.java
@@ -1,11 +1,28 @@
 package it.unicam.cs.ids.c3spa.core;
 
 
+import java.security.spec.KeySpec;
 import java.sql.*;
+import java.util.Base64;
+
 import it.unicam.cs.ids.c3spa.core.gestori.*;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESedeKeySpec;
 
 
 public class Servizi {
+    private static final String UNICODE_FORMAT = "UTF8";
+    public static final String DESEDE_ENCRYPTION_SCHEME = "DESede";
+    private KeySpec ks;
+    private SecretKeyFactory skf;
+    private Cipher cipher;
+    byte[] arrayBytes;
+    private String myEncryptionKey;
+    private String myEncryptionScheme;
+    private SecretKey key;
 
     public static String caricamento() throws SQLException {
 
@@ -118,6 +135,46 @@ public class Servizi {
     public static java.util.Date dataSqlToUtil(java.sql.Date data){
 
         return new java.util.Date(data.getTime());
+    }
+
+    public void TrippleDes() throws Exception {
+        myEncryptionKey = "SecretEcryptionKeySecretEncryptionKey";
+        myEncryptionScheme = DESEDE_ENCRYPTION_SCHEME;
+        arrayBytes = myEncryptionKey.getBytes(UNICODE_FORMAT);
+        ks = new DESedeKeySpec(arrayBytes);
+        skf = SecretKeyFactory.getInstance(myEncryptionScheme);
+        cipher = Cipher.getInstance(myEncryptionScheme);
+        key = skf.generateSecret(ks);
+    }
+
+
+    public String encrypt(String unencryptedString) throws Exception {
+        TrippleDes();
+        String encryptedString = null;
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] plainText = unencryptedString.getBytes(UNICODE_FORMAT);
+            byte[] encryptedText = cipher.doFinal(plainText);
+            encryptedString = new String(Base64.getEncoder().encode(encryptedText));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return encryptedString;
+    }
+
+
+    public String decrypt(String encryptedString) throws Exception {
+        TrippleDes();
+        String decryptedText=null;
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] encryptedText = Base64.getDecoder().decode(encryptedString);
+            byte[] plainText = cipher.doFinal(encryptedText);
+            decryptedText= new String(plainText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return decryptedText;
     }
 
 }
