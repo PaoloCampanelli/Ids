1239935d614900314dbb07d58905e150307a86b6
PaoloCampanelli <paolo.campanelli@studentiunicam.it>
2021-02-24 20:21:38 +0100

    code : sistemato il main

--- Project/it/unicam/cs/ids/c3spa/core/Main.java
+++ Project/it/unicam/cs/ids/c3spa/core/Main.java
@@ -41,29 +41,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println(Servizi.caricamento());
 
-        //Lettura del cliente numero 1
-        Cliente c = new GestoreCliente().getById(1);
-        Negozio n = new GestoreNegozio().getById(2);
-        Corriere cc = new GestoreCorriere().getById(1);
-        Pacco p =new GestorePacco().getById(1);
-        Amministratore a = new GestoreAmministratore().getById(1);
-
-        List<Cliente> lc = new GestoreCliente().getAll();
-        List<Negozio> ln = new GestoreNegozio().getAll();
-        List<Corriere> lcc = new GestoreCorriere().getAll();
-        List<Pacco> lp = new GestorePacco().getAll();
-
-
-        System.out.println(new GestorePacco().getByDestinatario(c));
-        System.out.println(new GestorePubblicita().getNegoziConPubblicitaAttivaByString("`indirizzo.provincia`", "MC"));
-        //System.out.println(new GestorePubblicita().OrderByPubblicita(new GestoreNegozio().getByCategoriaAndCitta("FRUTTA","2"),new GestorePubblicita().getNegoziConPubblicitaAttivaByCategoriaAndString("FRUTTA","`indirizzo.citta`", "2")));
-//        c.password = "654321";
-//        System.out.println(new GestoreCliente().cambiaPassword(c));
-
-        System.out.println(new GestoreSconto().getScontiAttiviByProvincia("MC"));
-
-
-
         launchGui();
 
        /*
